98d9814aed574c15441ac44d262ea3f7ae25d6fc
foreverddong <foreverddong@icloud.com>
2015-06-26 10:30:09 +0800

    Basic construction

---
+++ DanmakuClass/Server.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DanmakuClass
+{
+   
+    public partial class Server : Form
+    {
+        public DanmakuForm danmaku;
+        public DebugForm debug;
+        public Server()
+        {
+            InitializeComponent();
+            danmaku = new DanmakuForm();
+            debug = new DebugForm();
+            this.AddOwnedForm(debug);
+            this.AddOwnedForm(danmaku);
+        }
+
+        private void Server_Load(object sender, EventArgs e)
+        {
+            danmaku.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.debug.Show();
+        }
+    }
+}
---
+++ DanmakuClass/Utility.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Collections;
+using System;
+
+namespace DanmakuClass
+{
+    struct dustinPerson
+    {
+        public int speed;
+        public PictureBox dustinBox;
+    }
+     public struct Danmaku
+    { 
+        public System.String DanmakuString;
+        public Label DanmakuBox;
+    }
+}
